3b1b12eaf1e721de43a2dd42df99123662fc7711
Виген <vigenminasan67@gmail.com>
2024-11-07 00:50:35 +0500

    Добавил GET и POST запросы

--- dz.py
+++ dz.py
@@ -7,3 +7,18 @@ spisok = [
     {'id': 2, 'name': 'Антон', 'familia': 'Дегтярев'},
     {'id': 3, 'name': 'Артур', 'familia': 'Дегтярев'}
 ]
+
+@app.route('/zap', methods=['GET', 'POST'])
+def zap_func():
+    if request.method == 'GET':
+        return jsonify(spisok)
+    elif request.method == 'POST':
+        data = request.get_json() 
+        spis = {
+            'id': len(spisok) + 1,
+            'name': data['name'],  # Доступ к ключам из JSON
+            'familia': data['familia']
+        }
+        spisok.append(spis)
+        return jsonify(spis), 201
+
