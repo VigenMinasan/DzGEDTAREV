ec595be8c562680468554e546932be145afe6dd6
Виген <vigenminasan67@gmail.com>
2024-11-07 00:51:35 +0500

    Добавил UPDATE запрос

--- dz.py
+++ dz.py
@@ -45,3 +45,42 @@ def zapp_func(spisok_id):
     del spisok[spisok_id]
     return jsonify({'message': 'Данные удалены'}), 204
 
+
+@app.route('/zap', methods=['GET', 'POST'])
+def zap_func():
+    if request.method == 'GET':
+        return jsonify(spisok)
+    elif request.method == 'POST':
+        data = request.get_json() 
+        spis = {
+            'id': len(spisok) + 1,
+            'name': data['name'],  # Доступ к ключам из JSON
+            'familia': data['familia']
+        }
+        spisok.append(spis)
+        return jsonify(spis), 201
+
+@app.route('/del/<int:spisok_id>', methods=['PUT', 'DELETE'])
+def zapp_func(spisok_id):
+    spisok_id -= 1
+    if spisok_id < 0 or spisok_id >= len(spisok):
+        return jsonify({'error': 'Плохие данные'}), 404
+    del spisok[spisok_id]
+    return jsonify({'message': 'Данные удалены'}), 204
+
+@app.route('/update/<int:spisok_id>', methods=['PUT'])
+def update_func(spisok_id):
+    spisok_id -= 1
+    if spisok_id < 0 or spisok_id >= len(spisok):
+        return jsonify({'error': 'Плохие данные'}), 404
+
+    data = request.get_json()
+    spisok[spisok_id]['name'] = data.get('name', spisok[spisok_id]['name'])
+    spisok[spisok_id]['familia'] = data.get('familia', spisok[spisok_id]['familia'])
+    return jsonify(spisok[spisok_id]), 200
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
